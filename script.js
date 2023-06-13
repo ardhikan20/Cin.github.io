b0158e2407d29b108a3e2588cad49b290782fa67
ardhikan20 <89380339+ardhikan20@users.noreply.github.com>
2023-06-13 23:35:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Masukan Nama");
+ var b=prompt("Masukan Tanggal Lahir");
+  	if(a== "Cynthia" && b=="Marina")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		window.location.href="start.html";
+  	}
+  	
